ad1d812e39b8cecb061cfaae25e0dac0cc266815
lincoln <99297657+lincolncj@users.noreply.github.com>
2025-08-15 09:35:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function initPocketGame(config) {
 
   // Click anywhere on the fullscreen to close and reset
   fullscreen.addEventListener('click', () => {
-    if document.getElementById("zine-pages") return;
+    if (window.location.pathname === '/cobol.html') return;
     fullscreen.classList.remove('show');
     
     // Handle video if present (for Brian's page)
